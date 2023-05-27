afbbf3d4a1c482b8fe552ab7760c1fc46539df99
Lanin George <gmlanin@edu.hse.ru>
2023-05-27 21:48:03 +0300

    Changed and linted code for proba 2.

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -1,45 +1,30 @@
 """Custom LRUCache for caching data."""
-import collections
 from collections.abc import Hashable
 
 
 class LRUCache:
     """Class of custom LRU cache."""
-    OrderedItem = collections.namedtuple("OrderedItem", ["priority", "value"])
-
     def __init__(self, limit=42):
         if limit < 1:
             raise ValueError("LRU size should be greater than 0!")
-        self.__cache = {}
-        self.__capacity = limit
-        self.__length = 0
+        self.cache = {}
+        self.limit = limit
 
-    def __update_priority(self):
-        """Updates priority of all items and checks items for deletion."""
-        key_to_delete = None
-        for key, item in self.__cache.items():
-            self.__cache[key] = self.OrderedItem(item.priority+1, item.value)
-            if item.priority+1 == self.__capacity:
-                key_to_delete = key
-        return key_to_delete
-
-    def get(self, key):
+    def __getitem__(self, key):
         """Get value by key from cashe."""
-        try:
-            result = self.__cache[key].value
-            self.__update_priority()
-            self.__cache[key] = self.OrderedItem(0, self.__cache[key].value)
-            return result
-        except KeyError:
+        if key not in self.cache:
             return None
+        val = self.cache.pop(key)
+        self.cache[key] = val
+        return val
 
-    def set(self, key, value):
+    def __setitem__(self, key, value):
         """Add new key-value pair to cashe."""
         if not isinstance(key, Hashable):
             raise ValueError("Key should be hashable! Implement __hash__()!")
-        delete_key = self.__update_priority()
-        if self.__length < self.__capacity:
-            self.__length += 1
-        elif delete_key is not None:
-            del self.__cache[delete_key]
-        self.__cache[key] = self.OrderedItem(0, value)
+        if key in self.cache:
+            self.cache.pop(key)
+        else:
+            if len(self.cache) == self.limit:
+                del self.cache[next(iter(self.cache))]
+        self.cache[key] = value
--- 05/test_lru.py
+++ 05/test_lru.py
@@ -15,39 +15,63 @@ class TestLRU(unittest.TestCase):
     def test_get_none(self):
         """Tests get() with values which doesn't exist."""
         cache = LRUCache(5)
-        self.assertIsNone(cache.get("Not exist"))
-        cache.set("New key", 56)
-        self.assertIsNone(cache.get("Not exist"))
-
-    def test_priority_changes(self):
-        """Tests priority of values after get() and set() calls."""
-        cache = LRUCache(3)
-        self.assertEqual(cache._LRUCache__cache, {})
-        cache.set("First item", 345)
-        self.assertEqual(cache._LRUCache__cache["First item"].priority, 0)
-        cache.set("Second item", 45645)
-        self.assertEqual(cache._LRUCache__cache["First item"].priority, 1)
-        self.assertEqual(cache._LRUCache__cache["Second item"].priority, 0)
-        cache.get("First item")
-        self.assertEqual(cache._LRUCache__cache["First item"].priority, 0)
-        self.assertEqual(cache._LRUCache__cache["Second item"].priority, 1)
+        self.assertIsNone(cache["Not exist"])
+        cache["New key"] = 56
+        self.assertIsNone(cache["Not exist"])
 
     def test_unhashable_keys(self):
         """Tests that set() raises ValueError if key is unhashable."""
         cache = LRUCache()
         with self.assertRaises(ValueError):
-            cache.set([], "Some value")
+            cache[[]] = "Some value"
         with self.assertRaises(ValueError):
-            cache.set({}, "Some value")
+            cache[{}] = "Some value"
 
-    def test_overloaded(self):
+    def test_single_element_cache(self):
+        """Tests lru-cache with only one element."""
+        cache = LRUCache(1)
+        self.assertIsNone(cache["first"])
+        cache["first"] = 125
+        self.assertEqual(cache["first"], 125)
+        cache["second"] = "Second item"
+        self.assertIsNone(cache["first"])
+        self.assertEqual(cache["second"], "Second item")
+
+    def test_set_by_existant_key(self):
+        """Tests set method when cache is full."""
+        cache = LRUCache(2)
+        cache["k1"] = "val1"
+        cache["k2"] = "val2"
+        self.assertEqual(cache["k1"], "val1")
+        self.assertEqual(cache["k2"], "val2")
+        cache["k1"] = "new_val"
+        # Also two elements remain.
+        self.assertEqual(cache["k1"], "new_val")
+        self.assertEqual(cache["k2"], "val2")
+
+    def test_order_of_displacement(self):
+        """Tests order of displacement after get and set actions."""
+        cache = LRUCache(4)
+        cache["k1"] = "val1"
+        cache["k2"] = "val2"
+        cache["k3"] = "val3"
+        cache["k4"] = "val4"
+        cache["k5"] = "val5"
+        # val1 removed, because it was the oldest.
+        self.assertIsNone(cache["k1"])
+        self.assertEqual(cache["k2"], "val2")
+        # Now val2 is recently used, val3 will be removed next.
+        cache["k6"] = "val6"
+        self.assertIsNone(cache["k3"])
+
+    def test_overloaded_cache(self):
         """Tests cache when count of items become greater than capacity."""
         cache = LRUCache(2)
-        cache.set("k1", "val1")
-        cache.set("k2", "val2")
-        self.assertEqual(cache.get("k2"), "val2")
-        self.assertEqual(cache.get("k1"), "val1")
-        cache.set("k3", "val3")
-        self.assertEqual(cache.get("k3"), "val3")
-        self.assertIsNone(cache.get("k2"))
-        self.assertEqual(cache.get("k1"), "val1")
+        cache["k1"] = "val1"
+        cache["k2"] = "val2"
+        self.assertEqual(cache["k2"], "val2")
+        self.assertEqual(cache["k1"], "val1")
+        cache["k3"] = "val3"
+        self.assertEqual(cache["k3"], "val3")
+        self.assertIsNone(cache["k2"])
+        self.assertEqual(cache["k1"], "val1")
