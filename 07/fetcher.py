e8b83049347bbfc71deb689eef8c83b233c90853
Lanin George <gmlanin@edu.hse.ru>
2023-05-28 19:30:18 +0300

    Changed and linted code for proba 2.

--- 07/fetcher.py
+++ 07/fetcher.py
@@ -1,77 +1,59 @@
 """Async fetcher for urls."""
+import argparse
 import asyncio
 import time
-import sys
 import aiohttp
 
 
 FILE_NAME = 'urls.txt'
-WORKERS_COUNT = 4
-FILL_SIZE = 4
 
 
-def url_generator():
-    """Generator for reading urls row by row."""
-    file = open(FILE_NAME, 'r', encoding="utf-8")
-    with file:
-        while (line := file.readline().rstrip('\n')):
-            yield line
+def get_urls():
+    """This method returns all urls from file. Makes mocking easier."""
+    with open(FILE_NAME, "r", encoding="utf-8") as urls:
+        return urls.readlines()
 
 
-async def fetch_url(url, sem):
+async def fetch_url(session, sem, urls_queue):
     """Http request with semaphore."""
-    async with aiohttp.ClientSession() as session:
-        async with sem:
-            async with session.get(url) as resp:
-                print('I received: ', resp.status)
-
-
-async def async_worker(queue, semaphore):
-    """Worker for url from queue."""
     while True:
-        url = await queue.get()
+        url = await urls_queue.get()
         try:
-            await fetch_url(url, semaphore)
+            async with sem:
+                async with session.get(url) as resp:
+                    # First 50 letters of text.
+                    print('I received: ', str(await resp.text())[:50])
         finally:
-            queue.task_done()
+            urls_queue.task_done()
 
 
-async def process_links(semaphore_limit):
+async def process_links(semaphore_limit, workers_count=None):
     """Iterate on urls and create tasks for workers."""
     if not isinstance(semaphore_limit, int) or semaphore_limit < 1:
         raise ValueError("Limit should be positive integer!")
-    generator = url_generator()
     semaphore = asyncio.Semaphore(semaphore_limit)
-    queue = asyncio.Queue()
-    workers = [asyncio.create_task(async_worker(queue, semaphore))
-               for _ in range(WORKERS_COUNT)]
-    is_urls_exists = True
+    urls_queue = asyncio.Queue()
+    for url in get_urls():
+        await urls_queue.put(url)
+    if workers_count is None:
+        workers_count = urls_queue.qsize()
+    elif workers_count < 1:
+        raise ValueError("Count of workers can't be less than 1!")
     start = time.time()
-    while is_urls_exists:
-        # Fill the queue with new FILL_SIZE elements.
-        for _ in range(FILL_SIZE):
-            try:
-                queue.put_nowait(next(generator))
-            except StopIteration:
-                is_urls_exists = False
-        # Wait for workers.
-        await queue.join()
-
+    async with aiohttp.ClientSession() as session:
+        workers = [
+            asyncio.create_task(fetch_url(session, semaphore, urls_queue))
+            for _ in range(workers_count)
+        ]
+        await urls_queue.join()
+        for worker in workers:
+            worker.cancel()
     print('TIME', time.time() - start)
-
-    for worker in workers:
-        worker.cancel()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print('Specify limit of async requests!\n'
-              'For example: "python fetcher.py 5"')
-    else:
-        try:
-            LIMIT = int(sys.argv[1])
-            if LIMIT < 1:
-                raise ValueError()
-            asyncio.run(process_links(LIMIT))
-        except ValueError:
-            print('Limit should be positive integer!')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', type=int, choices=range(1, 21), default=4)
+    parser.add_argument('-w', type=int, choices=range(1, 50), required=False)
+    args = parser.parse_args()
+    asyncio.run(process_links(args.c, args.w))
--- 07/test_fetcher.py
+++ 07/test_fetcher.py
@@ -5,9 +5,12 @@ import asyncio
 import contextlib
 import random
 import time
-from fetcher import process_links
 
-test_links = {"https://www.python.org/",
+import aiohttp
+
+from fetcher import process_links, fetch_url
+
+TEST_LINKS = {"https://www.python.org/",
               "https://dart.dev/",
               "https://go.dev/",
               "https://kotlinlang.org/",
@@ -22,58 +25,94 @@ class TestAsyncFetcher(unittest.IsolatedAsyncioTestCase):
     """TestCase for async fetcher."""
     async def test_http_and_generator_mocks(self):
         """Tests calls for http get and url generator."""
-        with mock.patch("fetcher.url_generator", side_effect=generator_mock)\
+        with mock.patch("fetcher.get_urls", return_value = TEST_LINKS)\
              as mock_gen,\
              mock.patch("aiohttp.ClientSession.get", side_effect=HttpGetMock)\
              as http_mock:
             await process_links(5)
-            called_urls = set([x.args[0] for x in http_mock.call_args_list])
-            self.assertEqual(called_urls, test_links)
+            called_urls = {x.args[0] for x in http_mock.call_args_list}
+            self.assertEqual(called_urls, TEST_LINKS)
             mock_gen.assert_called_once_with()
 
     async def test_semaphore(self):
         """Tests impact of increasing semaphore limit."""
-        with mock.patch("fetcher.url_generator", side_effect=generator_mock),\
+        with mock.patch("fetcher.get_urls", return_value = TEST_LINKS),\
              mock.patch("aiohttp.ClientSession.get", side_effect=HttpGetMock):
             start = time.time()
-            await process_links(1)
+            await process_links(1, 10)
             slow_semaphore = time.time() - start
             start = time.time()
-            await process_links(9)
+            await process_links(5, 10)
             fast_semaphore = time.time() - start
-            # Sometimes it is not a truth, but on big numbers it works.
+            # Sometimes it is not a truth, but on big numbers it always works.
             self.assertGreaterEqual(slow_semaphore, fast_semaphore)
+
+    async def test_only_coroutine(self):
+        """Tests coroutine."""
+        with mock.patch("aiohttp.ClientSession.get", side_effect=HttpGetMock)\
+             as http_mock:
+            semaphore = asyncio.Semaphore(1)
+            urls_queue = asyncio.Queue()
+            for url in TEST_LINKS:
+                await urls_queue.put(url)
+            async with aiohttp.ClientSession() as session:
+                loop = asyncio.get_running_loop()
+                loop.create_task(fetch_url(session, semaphore, urls_queue))
+                await urls_queue.join()
+            called_urls = {x.args[0] for x in http_mock.call_args_list}
+            self.assertEqual(called_urls, TEST_LINKS)
 
     async def test_negative_semaphore(self):
         """Tests incorrect values for semaphore limit."""
-        with mock.patch("fetcher.url_generator", side_effect=generator_mock),\
+        with mock.patch("fetcher.get_urls", return_value = TEST_LINKS),\
              mock.patch("aiohttp.ClientSession.get", side_effect=HttpGetMock):
             with self.assertRaises(ValueError):
-                await process_links(-1)
+                await process_links(-1, 5)
             with self.assertRaises(ValueError):
-                await process_links(0)
+                await process_links(0, 5)
             with self.assertRaises(ValueError):
-                await process_links(-50)
+                await process_links(-50, 5)
 
+    async def test_negative_workers_count(self):
+        """Tests incorrect values for workers count."""
+        with mock.patch("fetcher.get_urls", return_value=TEST_LINKS),\
+             mock.patch("aiohttp.ClientSession.get", side_effect=HttpGetMock):
+            with self.assertRaises(ValueError):
+                await process_links(1, -50)
+            with self.assertRaises(ValueError):
+                await process_links(5, 0)
+            with self.assertRaises(ValueError):
+                await process_links(10, -1)
 
-def generator_mock():
-    """Mock generator without reading file."""
-    for link in test_links:
-        yield link
+    async def test_other_url_count(self):
+        """Tests process links with 3 urls."""
+        test_urls = {
+            "https://www.python.org/",
+            "https://dart.dev/",
+            "https://go.dev/"
+        }
+        with mock.patch("fetcher.get_urls", return_value = test_urls)\
+             as mock_gen,\
+             mock.patch("aiohttp.ClientSession.get", side_effect=HttpGetMock)\
+             as http_mock:
+            await process_links(5)
+            called_urls = {x.args[0] for x in http_mock.call_args_list}
+            self.assertEqual(called_urls, test_urls)
+            mock_gen.assert_called_once_with()
 
 
 class HttpGetMock(contextlib.AsyncContextDecorator):
     """Mock context decorator for aiohttp.ClientSession.get."""
     def __init__(self, url):
         self.url = url
-        self.status = f"200 {url}"
 
     async def __aenter__(self):
         await asyncio.sleep(random.random()/4)
         return self
 
-    def __str__(self):
-        return f"(Mock): {self.url}"
+    async def text(self):
+        """Mock text method."""
+        return f"Some text from {self.url}"
 
     async def __aexit__(self, *exc):
         return False
